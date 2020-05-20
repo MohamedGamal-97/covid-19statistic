9d6eaf0099de75031e83e8c6378913a6777a9811
mohamedgemy <midogemy100@gmail.com>
2020-05-20 05:33:03 +0200

    adding statistics each country

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { GlobalDataSummary } from 'src/app/models/global-data';
+import { DateWiseData } from 'src/app/models/date-wise-data';
+import { merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-countries',
@@ -15,17 +18,78 @@ export class CountriesComponent implements OnInit {
   totalActive=0;
   totalDeath=0;
   totalRecovered=0;
+  selectedCountryData:DateWiseData[];
+  dateWiseData;
+  dataTable=[];
+
+  loading=true;
+chart = {
+  PieChart : "PieChart" ,
+  ColumnChart : 'ColumnChart' ,
+  LineChart : "LineChart", 
+  height: 500, 
+  options: {
+    animation:{
+      duration: 1000,
+      easing: 'out',
+    },
+    is3D: true
+  }  
+}
+
   constructor(private service : DataServiceService) { }
 
+  // ngOnInit(): void {
+
+  //   this.service.getDateWiseData().subscribe(
+  //     (result)=>{
+  //       // console.log(result);
+  //       this.dateWiseData=result;
+  //     }
+  //   ),
+  //   this.service.getGlobalData().subscribe(result=>{
+  //     this.data=result;
+  //     this.data.forEach(cs=>{
+  //       this.countries.push(cs.country);
+  //     })
+  //   })
+  // }
+
   ngOnInit(): void {
-    this.service.getGlobalData().subscribe(result=>{
-      this.data=result;
-      this.data.forEach(cs=>{
-        this.countries.push(cs.country);
-      })
-    })
+
+    merge(
+      this.service.getDateWiseData().pipe(
+        map(result=>{
+          this.dateWiseData = result;
+        })
+      ), 
+      this.service.getGlobalData().pipe(map(result=>{
+        this.data = result;
+        this.data.forEach(cs=>{
+          this.countries.push(cs.country)
+        })
+      }))
+    ).subscribe(
+      {
+        complete : ()=>{
+         this.updateValues('India')
+         this.loading = false;
+        }
+      }
+    )
+    
+    
+
   }
 
+  updateChart(){
+    this.dataTable = [];
+    // this.dataTable.push(["Date" , 'Cases'])
+    this.dataTable.push('Cases')
+    this.selectedCountryData.forEach(cs=>{
+      this.dataTable.push(cs.cases)
+    })
+  }
   updateValues(country:string){
     console.log(country);
     this.data.forEach(cs=>{
@@ -36,6 +100,9 @@ export class CountriesComponent implements OnInit {
         this.totalRecovered=cs.recovered;
       }
     })
+  this.selectedCountryData=this.dateWiseData[country];
+  console.log(this.selectedCountryData)
+  this.updateChart();
   }
 
 }
