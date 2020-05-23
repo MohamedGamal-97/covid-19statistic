0b293b91d93398e7edabd4668cc311da5d658f95
mohamedgemy <midogemy100@gmail.com>
2020-05-23 07:02:50 +0200

    updated router for info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CountriesComponent } from './components/countries/countries.component';
+import { InfoComponent } from './components/info/info.component';
 
 
 const routes: Routes = [
   {path: '',component:HomeComponent},
-  {path:'countries',component:CountriesComponent}
+  {path:'countries',component:CountriesComponent},
+  {path:'Info',component:InfoComponent}
 ];
 
 @NgModule({
